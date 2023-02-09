3bb732db44221da9d383660ebd19bf7493d4443d
Shaghayegh <sha.karimi.n@gmail.com>
2023-02-09 19:32:33 +0100

    create paddles

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from turtle import Screen
-
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
+screen.listen()
 screen.bgcolor('black')
 screen.title('pong')
+screen.tracer(0)
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+
+
+screen.onkey(right_paddle.up, 'Up')
+screen.onkey(right_paddle.down, 'Down')
+screen.onkey(left_paddle.up, 'w')
+screen.onkey(left_paddle.down, 's')
 
 
 
+game_is_on = True
 
-
-
-
-
-
+while game_is_on:
+    screen.update()
 
 
 

822126c46fe31a20a44320d004b2bd26fca96d59
Shaghayegh <sha.karimi.n@gmail.com>
2023-02-09 19:48:44 +0100

    create ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -10,7 +12,7 @@ screen.tracer(0)
 
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 screen.onkey(right_paddle.up, 'Up')
 screen.onkey(right_paddle.down, 'Down')
@@ -22,7 +24,11 @@ screen.onkey(left_paddle.down, 's')
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move_the_ball()
+
+
 
 
 
